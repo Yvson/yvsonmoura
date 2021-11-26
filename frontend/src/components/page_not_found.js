0860407379ff506bbdc23fe39793960d18b99b55
Yvson Moura <yvsonexs@gmail.com>
2021-11-26 00:26:25 -0300

    Scroll bar was added to all pages.

--- frontend/src/components/page_not_found.js
+++ frontend/src/components/page_not_found.js
@@ -9,8 +9,8 @@ export default class PageNotFound extends Component {
             <div id="notfound-content" className="h-full flex flex-col flex-grow gap-4 justify-center items-center lg:flex-row lg:justify-evenly lg:items-center">
                 <div id="notfound-frame" className="gap-y-4 flex flex-col justify-center items-center">
                     <img className="w-auto lg:max-w-2xl" src={page_not_found} alt="404 Error" />
-                    <h1 className="title">Page Not Found</h1>
-                    <h4 className="paragraph-sm">This page does not exist!</h4>
+                    <h1 className="title">Page Not Found!</h1>
+                    <h4 className="paragraph-sm">This page does not exist.</h4>
                 </div>
             </div>
         )
